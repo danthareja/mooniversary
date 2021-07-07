9a925a033f95b8e536daa4bf2297606f1e3bfcd8
Dan Thareja <danthareja@gmail.com>
2021-07-07 10:23:16 -0600

    Comment env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    AUTH0_BASE_URL:
-      `https://${process.env.VERCEL_URL}` || process.env.AUTH0_BASE_URL,
-  },
+  // env: {
+  //   AUTH0_BASE_URL:
+  //     `https://${process.env.VERCEL_URL}` || process.env.AUTH0_BASE_URL,
+  // },
 };
