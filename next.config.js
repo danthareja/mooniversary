885914af53a5c9ad6d20581b19be5e186afdd84f
Dan Thareja <danthareja@gmail.com>
2021-07-07 10:33:20 -0600

    Try dynamic auth

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  // env: {
-  //   AUTH0_BASE_URL:
-  //     `https://${process.env.VERCEL_URL}` || process.env.AUTH0_BASE_URL,
-  // },
 };
--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,5 +1,4 @@
-// pages/api/auth/[...auth0].js
-import { handleAuth, handleLogout } from "@auth0/nextjs-auth0";
+import { initAuth0 } from "@auth0/nextjs-auth0";
 
 console.log("AUTH0_BASE_URL", process.env.AUTH0_BASE_URL);
 console.log("VERCEL_URL", process.env.VERCEL_URL);
@@ -7,14 +6,20 @@ console.log("VERCEL_ENV", process.env.VERCEL_ENV);
 console.log("NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
 console.log("NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
 
-export default handleAuth({
+// Workaround for dynamic AUTH0_BASE_URL env variable
+// https://github.com/auth0/nextjs-auth0/issues/383#issuecomment-828661649
+const auth0 = initAuth0({
+  baseURL: process.env.AUTH0_BASE_URL || process.env.VERCEL_URL,
+});
+
+export default auth0.handleAuth({
   async logout(req, res) {
     // You don't strictly need to sanitise `req.query.returnTo` because it has to be in Auth0's "Allowed Logout URLs"
     // But if you ever added a local logout option you should sanitise it, like we do with the login `returnTo`
     // eg https://github.com/auth0/nextjs-auth0/blob/beta/src/handlers/login.ts#L70-L72
     const returnTo = req.query.returnTo;
     try {
-      await handleLogout(req, res, {
+      await auth0.handleLogout(req, res, {
         returnTo,
       });
     } catch (error) {
