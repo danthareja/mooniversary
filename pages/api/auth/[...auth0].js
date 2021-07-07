64b39fa0c11d962dde93e956b39f58df95057822
Dan Thareja <danthareja@gmail.com>
2021-07-07 10:38:56 -0600

    Add protocol

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -9,7 +9,10 @@ console.log("NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
 // Workaround for dynamic AUTH0_BASE_URL env variable
 // https://github.com/auth0/nextjs-auth0/issues/383#issuecomment-828661649
 const auth0 = initAuth0({
-  baseURL: process.env.AUTH0_BASE_URL || process.env.VERCEL_URL,
+  baseURL:
+    process.env.AUTH0_BASE_URL ||
+    `https://${process.env.VERCEL_URL}` ||
+    "http://localhost:3000",
 });
 
 export default auth0.handleAuth({
