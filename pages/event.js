0ce1c542952fdc5043c7a16dcf2af50ba5ab665f
Dan Thareja <danthareja@gmail.com>
2021-08-30 19:06:55 -0400

    Add previous mooniversaries calculation

--- pages/event.js
+++ pages/event.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { format } from "date-fns";
-import { getNextFullMoon } from "@/lib/moon";
+import { getNextMooniversary } from "@/lib/mooniversary";
+
 import { EventInput } from "@/components";
 import styles from "@/styles/Home.module.css";
 
 export default function Event() {
-  const nextFullMoon = getNextFullMoon();
+  const nextMooniversary = getNextMooniversary();
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>{format(nextFullMoon, "yyyy/MM/dd")}</h1>
+        <h1 className={styles.title}>
+          {format(nextMooniversary, "yyyy/MM/dd")}
+        </h1>
         <p className={styles.description}>is our next Mooniversary</p>
-        <EventInput date={nextFullMoon} />
+        <EventInput date={nextMooniversary} />
       </main>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { format } from "date-fns";
-import { getNextFullMoon } from "@/lib/moon";
+import { getNextMooniversary } from "@/lib/mooniversary";
 import { MoonSlider, EventInput } from "@/components";
 import styles from "@/styles/Home.module.css";
 
 export default function Index() {
-  const nextFullMoon = getNextFullMoon();
+  const nextMooniversary = getNextMooniversary();
 
   return (
     <MoonSlider>
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1 className={styles.title}>{format(nextFullMoon, "yyyy/MM/dd")}</h1>
+          <h1 className={styles.title}>
+            {format(nextMooniversary, "yyyy/MM/dd")}
+          </h1>
           <p className={styles.description}>is our next Mooniversary</p>
-          <EventInput date={nextFullMoon} />
+          <EventInput date={nextMooniversary} />
         </main>
       </div>
     </MoonSlider>
