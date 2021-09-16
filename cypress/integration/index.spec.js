c92bb11f86d6e53b04905c6f5528a895f0e1c826
Dan Thareja <danthareja@gmail.com>
2021-09-15 21:50:25 -0400

    Add code coverage for the lulz (#6)

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,41 +1,48 @@
 describe("index page", () => {
-  it("should drag a moon to see the next mooniversary date", () => {
-    // Ideally we could assert the actual text of the mooniversary date
-    // using a cy.clock command like so:
-    //
-    // const now = new Date(Date.UTC(2017, 2, 14)).getTime();
-    // cy.clock(now);
-    //
-    // but right now this isn't working
-    // but maybe unit tests would be better here
+  // We cannot load JSON file using "cy.fixture"
+  // because it means the test is already running.
+  // Same with using "before" hook - new tests cannot be created from "before" hook.
+  // Instead we need to load JSON file using "require" at the start time
+  // and generate tests.
+  const moons = require("../fixtures/moons");
 
-    cy.visit("/");
+  moons.forEach((moon) => {
+    it(`should show the correct mooniversary on ${moon.clockDate}`, () => {
+      cy.clock(new Date(moon.clockDate), ["Date"]);
+      cy.visit("/");
 
-    cy.getEl("moon-ball").should("be.visible").as("ball");
-    cy.getEl("moon-basket").should("be.visible").as("basket");
+      cy.getEl("moon-ball").should("be.visible").as("ball");
+      cy.getEl("moon-basket").should("be.visible").as("basket");
 
-    cy.get("@ball").then(($ball) => {
-      cy.get("@basket").then(($basket) => {
-        const basket = $basket[0].getBoundingClientRect();
-        const offset = $ball.width() / 2;
-        cy.wrap($ball)
-          .trigger("mousedown", { button: 0 })
-          .trigger("mousemove", {
-            clientX: basket.x + offset,
-            clientY: basket.y + offset,
-          })
-          // For some reason, we need at least two mouse move events
-          // in order to trigger the screen correctly
-          // no idea why this could be a problem for future dan
-          .trigger("mousemove", {
-            clientX: basket.x + offset,
-            clientY: basket.y + offset,
-          });
+      // Drag and drop moon
+      cy.get("@ball").then(($ball) => {
+        cy.get("@basket").then(($basket) => {
+          const basket = $basket[0].getBoundingClientRect();
+          const offset = $ball.width() / 2;
+          cy.wrap($ball)
+            .trigger("mousedown", { button: 0 })
+            .trigger("mousemove", {
+              clientX: basket.x + offset,
+              clientY: basket.y + offset,
+            })
+            // For some reason, we need at least two mouse move events
+            // in order to trigger the screen correctly
+            // no idea why this could be a problem for future dan
+            .trigger("mousemove", {
+              clientX: basket.x + offset,
+              clientY: basket.y + offset,
+            });
+        });
       });
-    });
 
-    cy.getEl("next-mooniversary")
-      .findEl("next-mooniversary-date")
-      .findEl("next-mooniversary-number");
+      cy.getEl("next-mooniversary")
+        .findEl("next-mooniversary-date")
+        .should("contain.text", moon.mooniversaryDate)
+        .findEl("next-mooniversary-number")
+        .should(
+          "contain.text",
+          `will be our ${moon.mooniversaryNumber} Mooniversary`
+        );
+    });
   });
 });
