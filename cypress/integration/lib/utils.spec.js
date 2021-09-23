1bbec1dc8136eb154bfc72820eafe081f49eb453
Dan Thareja <danthareja@gmail.com>
2021-09-23 15:43:32 -0600

    Refactor test structure

--- cypress/integration/lib/utils.spec.js
+++ cypress/integration/lib/utils.spec.js
@@ -3,15 +3,10 @@ import { findMooniversaryDate } from "../../../lib/utils";
 
 describe("utils", () => {
   describe("#findMooniversaryDate", () => {
-    // We cannot load JSON file using "cy.fixture"
-    // because it means the test is already running.
-    // Same with using "before" hook - new tests cannot be created from "before" hook.
-    // Instead we need to load JSON file using "require" at the start time
-    // and generate tests.
     const mooniversaries = require("../../fixtures/mooniversaries");
 
     mooniversaries.forEach((mooniversary) => {
-      it(`should find mooniversary number ${mooniversary.number}`, () => {
+      it(`should find mooniversary #${mooniversary.number}`, () => {
         const actualDate = findMooniversaryDate(mooniversary.number);
         const expectedDate = parseISO(mooniversary.date);
         expect(actualDate.toString()).to.equal(expectedDate.toString());
