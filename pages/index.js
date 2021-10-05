169b58899132c5074519585c2690462d56dc877e
Dan Thareja <danthareja@gmail.com>
2021-10-04 23:22:32 -0600

    im sorry

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useMooniversary } from "@/lib/hooks";
-import { MoonSlider, EditText, EventInput } from "@/components";
+import { Background, MoonSlider, EditText, EventInput } from "@/components";
 import styles from "@/styles/Home.module.css";
 
 export default function Index() {
@@ -14,38 +14,41 @@ export default function Index() {
   } = useMooniversary();
 
   return (
-    <MoonSlider>
-      <div className={styles.container}>
-        <main className={styles.main} data-test="next-mooniversary">
-          <h1 className={styles.title} data-test="next-mooniversary-date">
-            {mooniversaryDateText}
-          </h1>
-          <div
-            className={styles.description}
-            data-test="next-mooniversary-number"
-          >
-            {mooniversaryNumberTextVerb}{" "}
-            <EditText
-              className={styles.number}
-              name="mooniversary"
-              type="number"
-              min="1"
-              max="999"
-              required
-              size={3}
-              autoComplete="off"
-              defaultValue={mooniversaryNumber.toString()}
-              label={mooniversaryNumberText}
-              onSave={({ value }) => {
-                const number = parseInt(value);
-                setMooniversaryNumber(number);
-              }}
-            />{" "}
-            Mooniversary
-          </div>
-          {/* <EventInput date={mooniversaryDate} /> */}
-        </main>
-      </div>
-    </MoonSlider>
+    <>
+      <Background />
+      <MoonSlider>
+        <div className={styles.container}>
+          <main className={styles.main} data-test="next-mooniversary">
+            <h1 className={styles.title} data-test="next-mooniversary-date">
+              {mooniversaryDateText}
+            </h1>
+            <div
+              className={styles.description}
+              data-test="next-mooniversary-number"
+            >
+              {mooniversaryNumberTextVerb}{" "}
+              <EditText
+                className={styles.number}
+                name="mooniversary"
+                type="number"
+                min="1"
+                max="999"
+                required
+                size={3}
+                autoComplete="off"
+                defaultValue={mooniversaryNumber.toString()}
+                label={mooniversaryNumberText}
+                onSave={({ value }) => {
+                  const number = parseInt(value);
+                  setMooniversaryNumber(number);
+                }}
+              />{" "}
+              Mooniversary
+            </div>
+            {/* <EventInput date={mooniversaryDate} /> */}
+          </main>
+        </div>
+      </MoonSlider>
+    </>
   );
 }
