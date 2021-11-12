f97b7963b8087655ac909c688cbc8d9a591c6d10
Dan Thareja <danthareja@gmail.com>
2021-11-12 12:58:16 -0700

    Add missing test case

--- cypress/integration/pages/index.spec.js
+++ cypress/integration/pages/index.spec.js
@@ -136,7 +136,7 @@ describe("index page", () => {
     });
   });
 
-  it("should change the mooniversary date format with a global method call", () => {
+  it.only("should change the mooniversary date format with a global method call", () => {
     // Subtract 7 days so relative dates don't come up
     cy.clock(sub(parseISO(earliestMooniversary.date), { days: 7 }), ["Date"]);
     cy.visit("/");
@@ -148,44 +148,51 @@ describe("index page", () => {
 
     cy.window().then((win) => {
       win.mooniversary.changeDateFormatTo("not a valid format");
-    });
-    cy.checkMooniversary({
-      dateText: earliestMooniversary.dateText,
+      cy.checkMooniversary({
+        dateText: earliestMooniversary.dateText,
+      });
     });
 
     cy.window().then((win) => {
       win.mooniversary.changeDateFormatTo("stupid american");
-    });
-    cy.checkMooniversary({
-      dateText: "05/26/2021",
+      cy.checkMooniversary({
+        dateText: "05/26/2021",
+      });
     });
 
     cy.window().then((win) => {
       win.mooniversary.changeDateFormatTo("proper south african");
-    });
-    cy.checkMooniversary({
-      dateText: "26/05/2021",
+      cy.checkMooniversary({
+        dateText: "26/05/2021",
+      });
     });
 
     cy.window().then((win) => {
       win.mooniversary.changeDateFormatTo("with the time");
-    });
-    cy.checkMooniversary({
-      dateText: "May 26th 2021 at 5:14:51 AM",
+      cy.checkMooniversary({
+        dateText: "May 26th 2021 at 5:14:51 AM",
+      });
     });
 
     cy.window().then((win) => {
       win.mooniversary.changeDateFormatTo("nerdy programmer");
-    });
-    cy.checkMooniversary({
-      dateText: "2021-05-26T11:14:51.093Z",
+      cy.checkMooniversary({
+        dateText: "2021-05-26T11:14:51.093Z",
+      });
     });
 
     cy.window().then((win) => {
       win.mooniversary.changeDateFormatTo("from now");
+      cy.checkMooniversary({
+        dateText: "in 7 days",
+      });
     });
-    cy.checkMooniversary({
-      dateText: "in 7 days",
+
+    cy.window().then((win) => {
+      win.mooniversary.changeDateFormatTo("original");
+      cy.checkMooniversary({
+        dateText: earliestMooniversary.dateText,
+      });
     });
   });
 });
